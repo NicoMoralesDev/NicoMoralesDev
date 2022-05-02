dec2ac5c5e85ae4f97362d5c22e0b0061d6bd2cd
NicolasMorales <nicomorales.dev@gmail.com>
2022-05-02 15:15:44 -0300

    Finished translations and minor style changes.

--- src/pages/resources/Resources.jsx
+++ src/pages/resources/Resources.jsx
@@ -33,36 +33,48 @@ const Resources = () => {
         <div className={styles.resourcesContainer}>
             <h2>{t("resources.heading")}</h2>
 
-            <h3>{t("resources.development")}</h3>
-            <p>{t("resources.developmentDesc")}</p>
+            <h3 className={styles.categoryTitle}>
+                {t("resources.development")}
+            </h3>
+            <p className={styles.categoryDesc}>
+                {t("resources.developmentDesc")}
+            </p>
             <CarouselContainer carouselList={developmentList} theme={theme} />
 
-            <h3>{t("resources.design")}</h3>
-            <p>{t("resources.designDesc")}</p>
+            <h3 className={styles.categoryTitle}>{t("resources.design")}</h3>
+            <p className={styles.categoryDesc}>{t("resources.designDesc")}</p>
             <CarouselContainer carouselList={designList} theme={theme} />
 
-            <h3>{t("resources.assets")}</h3>
-            <p>{t("resources.assetsDesc")}</p>
+            <h3 className={styles.categoryTitle}>{t("resources.assets")}</h3>
+            <p className={styles.categoryDesc}>{t("resources.assetsDesc")}</p>
             <CarouselContainer carouselList={assetsList} theme={theme} />
 
-            <h3>{t("resources.html-css")}</h3>
-            <p>{t("resources.html-cssDesc")}</p>
+            <h3 className={styles.categoryTitle}>{t("resources.html-css")}</h3>
+            <p className={styles.categoryDesc}>{t("resources.html-cssDesc")}</p>
             <CarouselContainer carouselList={htmlAndCssList} theme={theme} />
 
-            <h3>{t("resources.javascript")}</h3>
-            <p>{t("resources.javascriptDesc")}</p>
+            <h3 className={styles.categoryTitle}>
+                {t("resources.javascript")}
+            </h3>
+            <p className={styles.categoryDesc}>
+                {t("resources.javascriptDesc")}
+            </p>
             <CarouselContainer carouselList={javascriptList} theme={theme} />
 
-            {/* <h3>Java</h3>
-            <p>Java resources</p>
+            {/* <h3 className={styles.categoryTitle}>Java</h3>
+            <p className={styles.categoryDesc}>Java resources</p>
             <CarouselContainer carouselList={javaList} theme={theme} /> */}
 
-            <h3>{t("resources.deployment")}</h3>
-            <p>{t("resources.deploymentDesc")}</p>
+            <h3 className={styles.categoryTitle}>
+                {t("resources.deployment")}
+            </h3>
+            <p className={styles.categoryDesc}>
+                {t("resources.deploymentDesc")}
+            </p>
             <CarouselContainer carouselList={deploymentList} theme={theme} />
 
-            <h3>{t("resources.extras")}</h3>
-            <p>{t("resources.extrasDesc")}</p>
+            <h3 className={styles.categoryTitle}>{t("resources.extras")}</h3>
+            <p className={styles.categoryDesc}>{t("resources.extrasDesc")}</p>
             <CarouselContainer carouselList={extrasList} theme={theme} />
         </div>
     );
